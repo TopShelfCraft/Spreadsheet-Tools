9d327d9a7209171bec77a98eb8a245bfa39c9454
TJ Draper <tj@buzzingpixel.com>
2017-09-19 13:19:09 -0500

    Add initial settings model

---
+++ src/models/SettingsModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace topshelfcraft\excelimport\models;
+
+use craft\base\Model;
+
+/**
+ * Class SettingsModel
+ */
+class SettingsModel extends Model
+{
+    /** @var array $validImportTypes */
+    public $validImportTypes = [];
+}
