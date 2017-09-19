826d517cfccf21d33c2d45f4bd12d34b7c3ce466
TJ Draper <tj@buzzingpixel.com>
2017-09-19 11:39:42 -0500

    Move Excel Importer Service into correct place for Craft 3 composer loading

---
+++ src/services/HelperService.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace topshelfcraft\excelimport\services;
+
+use craft\base\Component;
+
+/**
+ * Class HelperService
+ * @property array $validImportTypes
+ */
+class HelperService extends Component
+{
+    /**
+     * Get the valid import types from the config
+     * @return array
+     */
+    public function getValidImportTypes() : array
+    {
+        return (array) craft()->config->get('validImportTypes', 'excelimporter');
+    }
+
+    /**
+     * Get the array keys for the valid import types
+     * @return array
+     */
+    public function getValidImportTypeKeys() : array
+    {
+        return array_keys($this->getValidImportTypes());
+    }
+
+    /**
+     * Determine if a string is a valid import type key.
+     * @param $importType
+     * @return bool
+     */
+    public function isValidImportType($importType) : bool
+    {
+        return in_array($importType, $this->getValidImportTypeKeys(), false);
+    }
+
+    /**
+     * Get the spreadsheet mime types from the config.
+     * @return mixed
+     */
+    public function getValidSpreadsheetMimes()
+    {
+        return craft()->config->get('validUploadMimes', 'excelimporter');
+    }
+
+    /**
+     * Determine if a string is a valid spreadsheet mime type.
+     * @param $mimeType
+     * @return bool
+     */
+    public function isValidSpreadsheetMime($mimeType) : bool
+    {
+        return in_array($mimeType, $this->getValidSpreadsheetMimes(), false);
+    }
+
+    /**
+     * Get the spreadsheet upload path.
+     * @param string $filename
+     * @return mixed
+     */
+    public function getSpreadsheetUploadPath($filename = '')
+    {
+        return craft()->config->get('xlsUploadPath', 'excelimporter') . trim($filename, '/');
+    }
+
+    /**
+     * Check if the spreadsheet exists.
+     * @param string $filename
+     * @return bool
+     */
+    public function doesSpreadsheetExist($filename = '') : bool
+    {
+        return ! empty($filename) ||
+            ! IOHelper::fileExists($this->getSpreadsheetUploadPath($filename));
+    }
+}
