778a38338adb8c9076a5c77942392c85d048c079
TJ Draper <tj@buzzingpixel.com>
2017-09-19 13:57:54 -0500

    Add additional settings to the Settings Model

--- src/models/SettingsModel.php
+++ src/models/SettingsModel.php
@@ -11,4 +11,13 @@ class SettingsModel extends Model
 {
     /** @var array $validImportTypes */
     public $validImportTypes = [];
+
+    /** @var array $validImportTypes */
+    public $validUploadMimes = [
+        'application/vnd.ms-excel',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    ];
+
+    /** @var string $xlsUploadPath */
+    public $xlsUploadPath = '';
 }
